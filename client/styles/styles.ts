d6a1cca49e830fae0bad2bd027db275f58465f1d
Karan Hejmadi <hejmadikaran9@gmail.com>
2021-11-13 11:23:22 +0530

    added styles, hook-form, styled-components (#1)

---
+++ client/styles/styles.ts
@@ -0,0 +1,33 @@
+export const badgeColor = {
+	lightGreen: "#8DFFAD",
+	darkGreen: "#026400",
+	lightRed: "#FFABBF",
+	darkRed: "#BC002D",
+	lightBlue: "#BEE8FA",
+	darkBlue: "#0089C3",
+	lightPurple: "#E4CEFF",
+	darkPurple: "#5600C3",
+	lightOrange: "#FFD39F",
+	darkOrange: "#D46600",
+};
+
+export const uiColor = {
+	white: "#FFFFFF", // background
+	indigo: "#2C3967", // table header, text color
+	green: "#17CEAD", // applied
+	darkBlue: "#7394F0", // reopen label, view details
+	blue: "#4BCDFF", // file drop icon
+	lightBlue: "#E6F7FF", // file drop background
+	red: "#FF0000", // student alert dot
+	darkRed: "#BC002D",
+	lightestGray: "#F1F6FA", // dark row color
+	lightGray: "#EBEDF0", // section border
+	gray: "#C4C4C4", // input border
+	darkGray: "#6A7782", // menu items
+	darkestGray: "#3B4854", // form labels, section heading
+};
+
+export const shadow = {
+	darkShadow: "box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);",
+	lightShadow: "box-shadow: 4px 4px 4px rgba(196, 196, 196, 0.25);",
+};
