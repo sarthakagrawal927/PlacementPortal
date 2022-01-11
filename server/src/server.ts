355d7a23762cca0c0848b97f7267b8be9e4a7e10
Karan Hejmadi <hejmadikaran9@gmail.com>
2022-01-11 20:44:00 +0530

    disabled introspection in production

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	context,
-	introspection: process.env.NODE_ENV === "development" ? true : false,
+	introspection: process.env.NODE_ENV !== "production",
 });
 
 server.listen({ port: PORT }, () => console.log(`🚀 Server listening at: http://localhost:${PORT}`));
